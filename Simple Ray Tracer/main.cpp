96f532b01d039e5c8f207d1f5b167b7e155259c8
tzhu65 <tzhu65@gmail.com>
2016-02-12 18:24:33 -0500

    Initial Commit

---
+++ Simple Ray Tracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simple Ray Tracer
+//
+//  Created by Tony Zhu on 2/12/16.
+//  Copyright © 2016 Tony Zhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
